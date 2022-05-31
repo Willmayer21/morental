1f0e87993a15c6debc5c20193a792ce44df547c4
Mohammad Chady <cma37@outlook.com>
2022-05-31 15:45:30 +0400

    branch3

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,7 +1,25 @@
 class RentalsController < ApplicationController
   def new
+    @rental = Rental.new
+    @car = Car.find(params[:car_id])
   end
 
   def create
+    @rental = Rental.new(rental_params)
+
+    @car = Car.find(params[:car_id])
+    @rental.car = @car
+    @rental.user = current user
+    if @rental.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:user_id, :car_id)
   end
 end
