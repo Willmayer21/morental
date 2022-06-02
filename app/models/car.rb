192cbca95d7f979d3152bb03c3b36ba36a3c1fbf
Will Mayer <william.mayer@outlook.com>
2022-06-02 14:22:32 +0400

    added search bar

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,10 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :rentals, dependent: :destroy
+  include PgSearch::Model
+  pg_search_scope :search_by_model_and_description,
+    against: [ :model, :description ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_01_101806) do
+ActiveRecord::Schema.define(version: 2022_06_02_082436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2022_06_01_101806) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "poster_url"
     t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "rentals", force: :cascade do |t|
@@ -58,6 +67,8 @@ ActiveRecord::Schema.define(version: 2022_06_01_101806) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "last_name"
+    t.string "first_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
